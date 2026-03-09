d8f68ecc85647ee74c9c07d608b216ca9ffe2dfe
soufiane <soufianeelbasraoui0@gmail.com>
2026-03-09 16:26:34 +0100

    initial commit

---
+++ src/main/java/org/example/supplyflow/SupplyFlowApplication.java
@@ -0,0 +1,17 @@
+package org.example.supplyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.List;
+
+@SpringBootApplication
+public class SupplyFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplyFlowApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/org/example/supplyflow/SupplyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.supplyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

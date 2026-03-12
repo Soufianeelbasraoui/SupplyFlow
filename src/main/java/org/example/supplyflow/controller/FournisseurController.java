438276c426d1c05076a3c010c38d39dce0e48e43
soufiane <soufianeelbasraoui0@gmail.com>
2026-03-12 03:58:49 +0100

    update

--- src/main/java/org/example/supplyflow/controller/FournisseurController.java
+++ src/main/java/org/example/supplyflow/controller/FournisseurController.java
@@ -1,4 +1,25 @@
 package org.example.supplyflow.controller;
 
+import org.example.supplyflow.service.FournisseurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fournisseurs")
 public class FournisseurController {
+
+    private FournisseurService fournisseurService;
+
+    public FournisseurController(FournisseurService fournisseurService) {
+        this.fournisseurService = fournisseurService;
+    }
+
+    @GetMapping
+    public String listFournisseur(Model model){
+     model.addAttribute("fournisseurs",fournisseurService.findAllFournisseur());
+     return "Fournisseurs/Liste-Fournisseurs";
+ }
 }
--- src/main/java/org/example/supplyflow/controller/ProduitController.java
+++ src/main/java/org/example/supplyflow/controller/ProduitController.java
@@ -1,5 +1,7 @@
 package org.example.supplyflow.controller;
 
+import org.example.supplyflow.model.Produit;
+import org.example.supplyflow.service.FournisseurService;
 import org.example.supplyflow.service.ProduitService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,15 +12,25 @@ import org.springframework.web.bind.annotation.*;
 public class ProduitController {
 
     private  ProduitService produitService;
+    private FournisseurService fournisseurService;
 
-    public ProduitController(ProduitService produitService) {
+
+
+    public ProduitController(ProduitService produitService,FournisseurService fournisseurService) {
         this.produitService = produitService;
+        this.fournisseurService = fournisseurService;
     }
 
     @GetMapping
     public String listProduits(Model model) {
         model.addAttribute("produits", produitService.getAllProduits());
         return "produits/list-produits";
+    }
+    @GetMapping ("/new")
+    public String addProduct(Model model){
+        model.addAttribute("produit",new Produit());
+        model.addAttribute("fournisseurs",fournisseurService.findAllFournisseur());
+        return "produits/add-produit";
     }
 
 }
